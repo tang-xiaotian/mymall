3f4c2a5aa80777a2309a4251760c0f9b6b110c80
txt <1003046928@qq.com>
2020-11-25 21:20:24 +0800

    浏览器标题，图标更改；navbar和网络组件添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,27 @@
 <template>
-<h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 
 export default {
   name: "Home",
-  components: {}
+  components: {
+    NavBar
+  }
 };
 </script>
 
 <style>
-
+  .home-nav{
+    background-color:var(--color-tint);
+    color: white;
+  }
 </style>
